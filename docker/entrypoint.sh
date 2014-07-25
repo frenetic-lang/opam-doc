96a968961abf41888e9b30cbb6f4725a495acac0
Arjun Guha <arjun.guha@gmail.com>
2014-07-25 09:56:35 -0400

    Hacks to build documentation on api

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,13 +2,19 @@
 set -x
 set -e
 
-export REPO=git@github.com:arjunguha/arjunguha.github.io.git
-export OPAMDOC_BASE_URI=http://arjunguha.github.io
+export REPO=git@github.com:frenetic-lang/api.git
+export OPAMDOC_BASE_URI=http://frenetic-lang.github.io/api/
 
 opam doc packet openflow topology frenetic || true
 
-git clone $(REPO)
-cd arjunguha.github.io
+cd /home/frenetic/.opam/doc/doc
+find . -name "*.html" | xargs -n 1 sed -i 's/\/js/http:\/\/frenetic-lang.github.io\/api\/js/g'
+find . -name "*.html" | xargs -n 1 sed -i 's/href="\/css/href="http:\/\/frenetic-lang.github.io\/api\/css/g'
+find . -name "*.html" | xargs -n 1 sed -i 's/href="\/"/href="http:\/\/frenetic-lang.github.io\/api"/g'
+
+cd /home/frenetic
+git clone -b gh-pages $REPO
+cd api
 rsync -av --delete --exclude=".git" /home/frenetic/.opam/doc/doc/ .
 git add --all
 
